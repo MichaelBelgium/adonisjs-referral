56f9ab0251a85df3995be4b29d2df16ec17c2fd6
Michael V. <michaelvrld@gmail.com>
2024-07-31 11:51:43 +0200

    Add hasReferrals mixin

---
+++ src/mixins/has_referrals.ts
@@ -0,0 +1,24 @@
+import { afterCreate, BaseModel, hasOne } from '@adonisjs/lucid/orm'
+import type { HasOne } from '@adonisjs/lucid/types/relations'
+import ReferralCode from '../models/referral_code.js'
+import { NormalizeConstructor } from '@adonisjs/core/types/helpers'
+
+export function hasReferrals() {
+  return <Model extends NormalizeConstructor<typeof BaseModel>>(superclass: Model) => {
+    class ModelWithReferrals extends superclass {
+      getModelId(): number {
+        throw new Error('Method getModelId not implemented.')
+      }
+
+      @hasOne(() => ReferralCode)
+      declare referralCode: HasOne<typeof ReferralCode>
+
+      @afterCreate()
+      static async createReferralCode(model: InstanceType<typeof ModelWithReferrals>) {
+        ReferralCode.create({ userId: model.getModelId() })
+      }
+    }
+
+    return ModelWithReferrals
+  }
+}
