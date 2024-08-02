fa8db67768808c2119828fc5775c0ce8a8c7049b
Michael V. <michaelvrld@gmail.com>
2024-08-02 15:55:44 +0200

    Add referrals table

---
+++ src/models/referral.ts
@@ -0,0 +1,32 @@
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import ReferralCode from './referral_code.js'
+import config from '@adonisjs/core/services/config'
+import { LucidModel } from '@adonisjs/lucid/types/model'
+
+export default class Referral extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare referralCodeId: number
+
+  @column()
+  declare referredUserId: number
+
+  @column({
+    consume: (value) => JSON.parse(value),
+    prepare: (value) => JSON.stringify(value),
+  })
+  declare metaData: object
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @belongsTo(() => ReferralCode)
+  declare referralCode: BelongsTo<typeof ReferralCode>
+
+  @belongsTo(() => config.get<LucidModel>('referral.userModel'))
+  declare referredUser: BelongsTo<LucidModel>
+}
--- src/models/referral_code.ts
+++ src/models/referral_code.ts
@@ -1,10 +1,18 @@
 import { DateTime } from 'luxon'
-import { afterCreate, BaseModel, beforeCreate, belongsTo, column } from '@adonisjs/lucid/orm'
+import {
+  afterCreate,
+  BaseModel,
+  beforeCreate,
+  belongsTo,
+  column,
+  hasMany,
+} from '@adonisjs/lucid/orm'
 import ReferralCodeGenerator from '../helpers/referral_code_generator.js'
 import emitter from '@adonisjs/core/services/emitter'
-import { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 import config from '@adonisjs/core/services/config'
 import { LucidModel } from '@adonisjs/lucid/types/model'
+import { Referral } from '../../index.js'
 
 export default class ReferralCode extends BaseModel {
   @column({ isPrimary: true })
@@ -24,6 +32,9 @@ export default class ReferralCode extends BaseModel {
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
+
+  @hasMany(() => Referral)
+  declare referrals: HasMany<typeof Referral>
 
   @beforeCreate()
   static async createReferralCode(referralCode: ReferralCode) {
