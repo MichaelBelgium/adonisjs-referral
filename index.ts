b0d2b14f057e1d9f70c2708b44216ea1f17d8ebe
Michael V. <michaelvrld@gmail.com>
2024-08-02 15:56:03 +0200

    export referralcode, referral models

--- index.ts
+++ index.ts
@@ -7,5 +7,12 @@
 |
 */
 
+import modelReferral from './src/models/referral.js'
+import modelReferralCode from './src/models/referral_code.js'
+
 export { configure } from './configure.js'
+
+export const Referral = modelReferral
+export const ReferralCode = modelReferralCode
+
 export { hasReferrals } from './src/mixins/has_referrals.js'
